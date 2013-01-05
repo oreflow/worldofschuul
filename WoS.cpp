2c2986a35acdebec960503f79e8e4a94ae165ce6
Tim Malmström <tim@GORTHAUR.(none)>
2013-01-05 13:38:27 +0100

    added WoS.cpp, som main fil istället för att ha main i game.h

---
+++ WoS.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdlib.h>
+#include "game.h"
+#include "player.h"
+
+using namespace std;
+using namespace WoS;
+int main()
+	{
+		bool active = true;
+		string str;
+		cout << "You have entered the World of Schuul \nyour goal in this game is to learn stuff\nto your help you have the following commands:\ngo <direction>\nsearch\npick up <item>\ndrop <item>\nuse <item> on <target>\n" << endl;
+		cout << "Enter your name: " << endl;
+		cin >> str;
+		Game g;
+		Player* p = new Player(&g, str);
+		g.setPlayer(p);
+
+		while(active)
+		{
+		cin >> str;
+		
+		if(str.compare("exit") == 0 ||str.compare("quit") == 0 )
+		  {	
+			cout << "Exiting the World of Schuul" << endl;
+			active = false;
+		  }
+		else
+			g.runCommand(str);
+		
+		}
+
+		return 0;
+	}
+
+
